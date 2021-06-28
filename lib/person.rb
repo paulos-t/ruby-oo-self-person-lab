effcdfcafc473d503dd8c36bd6ce04ffabf0644a
Paulos Thomas <paulost72@gmail.com>
2021-06-28 07:23:44 -0700

    add Person class with modified setter methods for instance variables

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,66 @@
 # your code goes here
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    def happiness=(new_happiness)
+        if new_happiness > 10
+            @happiness = 10
+        elsif new_happiness < 0
+            @happiness = 0
+        else
+            @happiness = new_happiness
+        end
+    end
+    def hygiene=(new_hygiene)
+        if new_hygiene > 10
+            @hygiene = 10
+        elsif new_hygiene < 0
+            @hygiene = 0
+        else
+            @hygiene = new_hygiene
+        end
+    end
+    def happy?
+        self.happiness > 7
+    end
+    def clean?
+        self.hygiene > 7
+    end
+    def get_paid(salary)
+        self.bank_account += salary
+        "all about the benjamins"
+    end
+    def take_bath
+        self.hygiene = self.hygiene + 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+    def work_out
+        self.happiness = self.happiness + 2
+        self.hygiene = self.hygiene - 3
+        "♪ another one bites the dust ♫"
+    end
+    def call_friend(friend)
+        self.happiness = self.happiness + 3
+        friend.happiness = friend.happiness + 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness = self.happiness - 2
+            friend.happiness = friend.happiness - 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness = self.happiness + 1
+            friend.happiness = friend.happiness + 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end
